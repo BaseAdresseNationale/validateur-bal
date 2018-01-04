ece326c44bc98eeb8f7ad9c097d853d841f12b9a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-01-04 17:10:31 +0100

    Add minimal test

---
+++ test/validate.js
@@ -0,0 +1,19 @@
+const {join} = require('path')
+const fs = require('fs')
+const {promisify} = require('util')
+const test = require('ava')
+
+const {validate} = require('../')
+
+const readFile = promisify(fs.readFile)
+
+async function readAsBuffer(relativePath) {
+  const absolutePath = join(__dirname, 'data', relativePath)
+  return readFile(absolutePath)
+}
+
+test('validate a file', async t => {
+  const buffer = await readAsBuffer('sample.csv')
+  const report = await validate(buffer)
+  t.is(report.parseMeta.encoding, 'UTF-8')
+})
