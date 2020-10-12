9773e138310dcc22fe7099cc14c6df0b20bc21b0
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-10-12 22:12:47 +0200

    Fix suffixe validation

--- test/schema.js
+++ test/schema.js
@@ -14,13 +14,13 @@ test('validate suffixe', t => {
     warnings: ['La valeur du champ suffixe ne doit pas avoir d’espaces en début ou en fin de chaîne de caractère']
   })
 
-  t.deepEqual(fields.suffixe.parse(' 1'), {
-    errors: ['La valeur du champ suffixe doit commencer par un caractère alphabétique.'],
+  t.deepEqual(fields.suffixe.parse(' .'), {
+    errors: ['La valeur du champ suffixe doit commencer par un caractère alphanumérique.'],
     warnings: ['La valeur du champ suffixe ne doit pas avoir d’espaces en début ou en fin de chaîne de caractère']
   })
 
-  t.deepEqual(fields.suffixe.parse('1'), {
-    errors: ['La valeur du champ suffixe doit commencer par un caractère alphabétique.'],
+  t.deepEqual(fields.suffixe.parse('.'), {
+    errors: ['La valeur du champ suffixe doit commencer par un caractère alphanumérique.'],
     warnings: []
   })
 })
