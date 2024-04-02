1963adaea0f19b1103c6c49a658f97823a180daf
Fufeck <fabien.tafforeau@gmail.com>
2024-04-02 17:58:34 +0200

    lint: add eslint and prettier

--- scripts/build-minicog.js
+++ scripts/build-minicog.js
@@ -1,24 +1,31 @@
 #!/usr/bin/env node
-const path = require('path')
-const process = require('process')
-const {outputJson} = require('fs-extra')
-const communes = require('@etalab/decoupage-administratif/data/communes.json')
+const path = require("path");
+const process = require("process");
+const { outputJson } = require("fs-extra");
+const communes = require("@etalab/decoupage-administratif/data/communes.json");
 
-const PLM = new Set(['75056', '69123', '13055'])
+const PLM = new Set(["75056", "69123", "13055"]);
 
 async function main() {
   const communesActuelles = communes
-    .filter(c => ['commune-actuelle', 'arrondissement-municipal'].includes(c.type) && !PLM.has(c.code))
-    .map(c => ({code: c.code, nom: c.nom, anciensCodes: c.anciensCodes}))
+    .filter(
+      (c) =>
+        ["commune-actuelle", "arrondissement-municipal"].includes(c.type) &&
+        !PLM.has(c.code),
+    )
+    .map((c) => ({ code: c.code, nom: c.nom, anciensCodes: c.anciensCodes }));
 
   const communesDelegueesAssociees = communes
-    .filter(c => ['commune-associee', 'commune-deleguee'].includes(c.type))
-    .map(c => ({code: c.code, nom: c.nom, chefLieu: c.chefLieu}))
+    .filter((c) => ["commune-associee", "commune-deleguee"].includes(c.type))
+    .map((c) => ({ code: c.code, nom: c.nom, chefLieu: c.chefLieu }));
 
-  await outputJson(path.join(__dirname, '..', 'minicog.json'), [...communesActuelles, ...communesDelegueesAssociees])
+  await outputJson(path.join(__dirname, "..", "minicog.json"), [
+    ...communesActuelles,
+    ...communesDelegueesAssociees,
+  ]);
 }
 
-main().catch(error => {
-  console.error(error)
-  process.exit(1)
-})
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
