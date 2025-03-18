51130eefc72418a34f0488912cc4de13e175095a
Fufeck <fabien.tafforeau@gmail.com>
2025-03-18 11:00:27 +0100

    export type

--- lib/index.ts
+++ lib/index.ts
@@ -3,9 +3,20 @@ import { validateProfile } from './validate/profiles';
 import { readValue } from './validate/rows';
 import { getErrorLevel, getLabel } from './utils/helpers';
 import profiles from './schema/profiles';
-import { ProfileType } from './schema/profiles/profile.type';
-import {
-  ErrorLevelEnum,
+
+export {
+  validate,
+  validateProfile,
+  prevalidate,
+  getLabel,
+  readValue,
+  getErrorLevel,
+  profiles,
+};
+
+export { ErrorLevelEnum } from './validate/validate.type';
+
+export type {
   PrevalidateType,
   ValidateProfileType,
   FieldType,
@@ -18,24 +29,4 @@ import {
   ProfileErrorType,
 } from './validate/validate.type';
 
-export {
-  validate,
-  validateProfile,
-  prevalidate,
-  getLabel,
-  readValue,
-  getErrorLevel,
-  profiles,
-  ProfileType,
-  ErrorLevelEnum,
-  PrevalidateType,
-  ValidateProfileType,
-  FieldType,
-  NotFoundFieldType,
-  ValidateRowType,
-  ValueIsValidType,
-  ValidateFileType,
-  ProfilesValidationType,
-  ParseFileType,
-  ProfileErrorType,
-};
+export type { ProfileType } from './schema/profiles/profile.type';
