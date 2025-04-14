7885b68668a8fe912970580b43130503e6a0f1b9
Fufeck <fabien.tafforeau@gmail.com>
2025-04-14 11:32:46 +0200

    correct test

--- lib/schema/__tests__/rows.spec.ts
+++ lib/schema/__tests__/rows.spec.ts
@@ -1,7 +1,19 @@
+import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
 import validateRows from '../rows';
 
+enableFetchMocks();
+
 describe('VALIDATE ROWS', () => {
-  it('TEST no errors', () => {
+  beforeEach(() => {
+    fetchMock.doMock(async () => {
+      return JSON.stringify({
+        status: 'success',
+        response: [{ id: '0246e48c-f33d-433a-8984-034219be842e' }],
+      });
+    });
+  });
+
+  it('TEST no errors', async () => {
     const errors: string[] = [];
     const rows: any[] = [
       {
@@ -9,43 +21,35 @@ describe('VALIDATE ROWS', () => {
           voie_nom: 'rue du Colombier',
           numero: 1,
           lieudit_complement_nom: 'paradis',
+          commune_insee: '91534',
         },
+        remediations: {},
       },
       {
         parsedValues: {
           voie_nom: 'paradis',
           numero: 99999,
+          commune_insee: '91534',
         },
+        remediations: {},
       },
     ];
-    validateRows(
-      rows,
-      {
-        addError: (e: string) => errors.push(e),
-      },
-      {
-        communeBanIds: [],
-      },
-    );
+    await validateRows(rows, {
+      addError: (e: string) => errors.push(e),
+    });
     expect(errors).toEqual([]);
   });
 
-  it('TEST rows.empty', () => {
+  it('TEST rows.empty', async () => {
     const errors: string[] = [];
     const rows: any[] = [];
-    validateRows(
-      rows,
-      {
-        addError: (e: string) => errors.push(e),
-      },
-      {
-        communeBanIds: [],
-      },
-    );
+    await validateRows(rows, {
+      addError: (e: string) => errors.push(e),
+    });
     expect(errors).toContain('rows.empty');
   });
 
-  it('TEST no ban_ids', () => {
+  it('TEST no ban_ids', async () => {
     const errors: string[] = [];
     const rows: any[] = [
       {
@@ -54,8 +58,10 @@ describe('VALIDATE ROWS', () => {
           id_ban_toponyme: '',
           id_ban_adresse: '',
           voie_nom: 'rue du Colombier',
+          commune_insee: '91534',
           numero: 1,
         },
+        remediations: {},
       },
       {
         parsedValues: {
@@ -63,23 +69,19 @@ describe('VALIDATE ROWS', () => {
           id_ban_toponyme: '',
           id_ban_adresse: '',
           voie_nom: 'rue du Colombier',
+          commune_insee: '91534',
           numero: 2,
         },
+        remediations: {},
       },
     ];
-    validateRows(
-      rows,
-      {
-        addError: (e: string) => errors.push(e),
-      },
-      {
-        communeBanIds: [],
-      },
-    );
+    await validateRows(rows, {
+      addError: (e: string) => errors.push(e),
+    });
     expect(errors).toEqual([]);
   });
 
-  it('TEST no error ban_ids', () => {
+  it('TEST no error ban_ids', async () => {
     const errors: string[] = [];
     const rows: any[] = [
       {
@@ -88,8 +90,10 @@ describe('VALIDATE ROWS', () => {
           id_ban_toponyme: '0246e48c-f33d-433a-8984-034219be842e',
           id_ban_adresse: '0246e48c-f33d-433a-8984-034219be842e',
           voie_nom: 'rue du Colombier',
+          commune_insee: '91534',
           numero: 1,
         },
+        remediations: {},
       },
       {
         parsedValues: {
@@ -97,23 +101,19 @@ describe('VALIDATE ROWS', () => {
           id_ban_toponyme: '0246e48c-f33d-433a-8984-034219be842e',
           id_ban_adresse: '',
           voie_nom: 'rue du Colombier',
+          commune_insee: '91534',
           numero: 99999,
         },
+        remediations: {},
       },
     ];
-    validateRows(
-      rows,
-      {
-        addError: (e: string) => errors.push(e),
-      },
-      {
-        communeBanIds: ['0246e48c-f33d-433a-8984-034219be842e'],
-      },
-    );
+    await validateRows(rows, {
+      addError: (e: string) => errors.push(e),
+    });
     expect(errors).toEqual([]);
   });
 
-  it('TEST rows.multi_id_ban_commune', () => {
+  it('TEST rows.multi_id_ban_commune', async () => {
     const errors: string[] = [];
     const rows: any[] = [
       {
@@ -122,8 +122,10 @@ describe('VALIDATE ROWS', () => {
           id_ban_toponyme: '0246e48c-f33d-433a-8984-034219be842e',
           id_ban_adresse: '0246e48c-f33d-433a-8984-034219be842e',
           voie_nom: 'rue du Colombier',
+          commune_insee: '91534',
           numero: 1,
         },
+        remediations: {},
       },
       {
         parsedValues: {
@@ -131,26 +133,19 @@ describe('VALIDATE ROWS', () => {
           id_ban_toponyme: '8a3bab10-f329-4ce3-9c7d-280d91a8053a',
           id_ban_adresse: '',
           voie_nom: 'rue du Colombier',
+          commune_insee: '91534',
           numero: 99999,
         },
+        remediations: {},
       },
     ];
-    validateRows(
-      rows,
-      {
-        addError: (e: string) => errors.push(e),
-      },
-      {
-        communeBanIds: [
-          '8a3bab10-f329-4ce3-9c7d-280d91a8053a',
-          '0246e48c-f33d-433a-8984-034219be842e',
-        ],
-      },
-    );
+    await validateRows(rows, {
+      addError: (e: string) => errors.push(e),
+    });
     expect(errors).toContain('rows.multi_id_ban_commune');
   });
 
-  it('TEST rows.every_line_required_id_ban', () => {
+  it('TEST rows.every_line_required_id_ban', async () => {
     const errors: string[] = [];
     const rows: any[] = [
       {
@@ -159,29 +154,27 @@ describe('VALIDATE ROWS', () => {
           id_ban_toponyme: '0246e48c-f33d-433a-8984-034219be842e',
           id_ban_adresse: '0246e48c-f33d-433a-8984-034219be842e',
           voie_nom: 'rue du Colombier',
+          commune_insee: '91534',
           numero: 1,
         },
+        remediations: {},
       },
       {
         parsedValues: {
           voie_nom: 'rue du Colombier',
+          commune_insee: '91534',
           numero: 2,
         },
+        remediations: {},
       },
     ];
-    validateRows(
-      rows,
-      {
-        addError: (e: string) => errors.push(e),
-      },
-      {
-        communeBanIds: [],
-      },
-    );
+    await validateRows(rows, {
+      addError: (e: string) => errors.push(e),
+    });
     expect(errors).toContain('rows.every_line_required_id_ban');
   });
 
-  it('TEST uuid_adresse rows.every_line_required_id_ban', () => {
+  it('TEST uuid_adresse rows.every_line_required_id_ban', async () => {
     const errors: string[] = [];
     const rows: any[] = [
       {
@@ -195,28 +188,26 @@ describe('VALIDATE ROWS', () => {
         parsedValues: {
           voie_nom: 'rue du Colombier',
           numero: 1,
+          commune_insee: '91534',
         },
+        remediations: {},
       },
       {
         parsedValues: {
           voie_nom: 'rue du Colombier',
           numero: 2,
+          commune_insee: '91534',
         },
+        remediations: {},
       },
     ];
-    validateRows(
-      rows,
-      {
-        addError: (e: string) => errors.push(e),
-      },
-      {
-        communeBanIds: ['0246e48c-f33d-433a-8984-034219be842e'],
-      },
-    );
+    await validateRows(rows, {
+      addError: (e: string) => errors.push(e),
+    });
     expect(errors).toContain('rows.every_line_required_id_ban');
   });
 
-  it('TEST rows.cog_no_match_id_ban_commune', () => {
+  it('TEST rows.cog_no_match_id_ban_commune', async () => {
     const errors: string[] = [];
     const rows: any[] = [
       {
@@ -230,7 +221,9 @@ describe('VALIDATE ROWS', () => {
         parsedValues: {
           voie_nom: 'rue du Colombier',
           numero: 1,
+          commune_insee: '91534',
         },
+        remediations: {},
       },
       {
         additionalValues: {
@@ -243,63 +236,14 @@ describe('VALIDATE ROWS', () => {
         parsedValues: {
           voie_nom: 'rue du Colombier',
           numero: 2,
+          commune_insee: '91534',
         },
+        remediations: {},
       },
     ];
-    validateRows(
-      rows,
-      {
-        addError: (e: string) => errors.push(e),
-      },
-      {
-        communeBanIds: ['0246e48c-f33d-433a-8984-034219be842e'],
-      },
-    );
+    await validateRows(rows, {
+      addError: (e: string) => errors.push(e),
+    });
     expect(errors).toContain('rows.cog_no_match_id_ban_commune');
-  });
-
-  it('TEST no error rows.cog_no_match_id_ban_commune', () => {
-    const errors: string[] = [];
-    const rows: any[] = [
-      {
-        additionalValues: {
-          uid_adresse: {
-            idBanCommune: '0246e48c-f33d-433a-8984-034219be842e',
-            idBanToponyme: '0246e48c-f33d-433a-8984-034219be842e',
-            idBanAdresse: '0246e48c-f33d-433a-8984-034219be842e',
-          },
-        },
-        parsedValues: {
-          voie_nom: 'rue du Colombier',
-          numero: 1,
-        },
-      },
-      {
-        additionalValues: {
-          uid_adresse: {
-            idBanCommune: '0246e48c-f33d-433a-8984-034219be842a',
-            idBanToponyme: '0246e48c-f33d-433a-8984-034219be842e',
-            idBanAdresse: '0246e48c-f33d-433a-8984-034219be842e',
-          },
-        },
-        parsedValues: {
-          voie_nom: 'rue du Colombier',
-          numero: 2,
-        },
-      },
-    ];
-    validateRows(
-      rows,
-      {
-        addError: (e: string) => errors.push(e),
-      },
-      {
-        communeBanIds: [
-          '0246e48c-f33d-433a-8984-034219be842e',
-          '0246e48c-f33d-433a-8984-034219be842a',
-        ],
-      },
-    );
-    expect(errors).not.toContain('rows.cog_no_match_id_ban_commune');
   });
 });
