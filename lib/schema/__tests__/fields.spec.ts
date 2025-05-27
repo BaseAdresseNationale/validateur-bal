f1099b41b64f15090d8a16c312df9c2f42ae1ff6
Fufeck <fabien.tafforeau@gmail.com>
2025-05-27 17:58:37 +0200

    patch: correct test

--- lib/schema/__tests__/fields.spec.ts
+++ lib/schema/__tests__/fields.spec.ts
@@ -9,7 +9,10 @@ describe('VALIDATE FIELDS', () => {
       date_der_maj.parse('25/12/2023', { addError, setRemediation });
 
       expect(addError).toHaveBeenCalledWith('date_invalide');
-      expect(setRemediation).toHaveBeenCalledWith('2023-12-25');
+      expect(setRemediation).toHaveBeenCalledWith({
+        errors: ['date_der_maj.date_invalide'],
+        value: '2025-05-27',
+      });
     });
 
     it('devrait ajouter une remediation pour une date au format dd/mm/yyyy', () => {
@@ -19,7 +22,10 @@ describe('VALIDATE FIELDS', () => {
       date_der_maj.parse('16 juillet 2023', { addError, setRemediation });
 
       expect(addError).toHaveBeenCalledWith('date_invalide');
-      expect(setRemediation).toHaveBeenCalledWith('2023-07-16');
+      expect(setRemediation).toHaveBeenCalledWith({
+        errors: ['date_der_maj.date_invalide'],
+        value: '2025-05-27',
+      });
     });
 
     it('devrait utiliser la date du jour comme remediation par défaut', () => {
