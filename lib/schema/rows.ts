3ca68c56336423929f2833b3c5fbdbbdd5481ccc
Fufeck <fabien.tafforeau@gmail.com>
2025-07-16 15:50:40 +0200

    add try catch to turf function

--- lib/schema/rows.ts
+++ lib/schema/rows.ts
@@ -195,33 +195,37 @@ function getFeaturesPointsByVoie(
 }
 
 function validateRowsCoords(rows: ValidateRowType[]) {
-  // On créer un répètoire de features de points par voie
-  const pointsByVoie = getFeaturesPointsByVoie(rows);
+  try {
+    // On créer un répètoire de features de points par voie
+    const pointsByVoie = getFeaturesPointsByVoie(rows);
 
-  for (const row of rows) {
-    if (
-      row.parsedValues.numero === 99_999 ||
-      !row.parsedValues.long ||
-      !row.parsedValues.lat
-    ) {
-      continue;
+    for (const row of rows) {
+      if (
+        row.parsedValues.numero === 99_999 ||
+        !row.parsedValues.long ||
+        !row.parsedValues.lat
+      ) {
+        continue;
+      }
+      // On récupère les coordonnées du point de la ligne
+      const currentPoint = turf.point([
+        row.parsedValues.long,
+        row.parsedValues.lat,
+      ]);
+      // On calcule chaques distance avec les autres points de la même voie (sauf 0 le même point)
+      const voie = getVoieIdentifier(row);
+      const distances = pointsByVoie[voie]
+        .map((otherPoint) => turf.distance(currentPoint, otherPoint))
+        .filter((distance) => distance > 0);
+      // Si le point de la ligne esT distant de plus 1km de tous les autres points de la même voie
+      if (distances.length > 0 && Math.min(...distances) > 1) {
+        row.errors?.push({
+          code: 'row.coord_outlier',
+        });
+      }
     }
-    // On récupère les coordonnées du point de la ligne
-    const currentPoint = turf.point([
-      row.parsedValues.long,
-      row.parsedValues.lat,
-    ]);
-    // On calcule chaques distance avec les autres points de la même voie (sauf 0 le même point)
-    const voie = getVoieIdentifier(row);
-    const distances = pointsByVoie[voie]
-      .map((otherPoint) => turf.distance(currentPoint, otherPoint))
-      .filter((distance) => distance > 0);
-    // Si le point de la ligne esT distant de plus 1km de tous les autres points de la même voie
-    if (distances.length > 0 && Math.min(...distances) > 1) {
-      row.errors?.push({
-        code: 'row.coord_outlier',
-      });
-    }
+  } catch (error) {
+    console.error('Problème lors de la validation des coordonnées', error);
   }
 }
 
@@ -229,40 +233,44 @@ function validateRowsCadastreNextToLongLat(
   rows: ValidateRowType[],
   { cadastreGeoJSON }: { cadastreGeoJSON: FeatureCollection },
 ) {
-  for (const row of rows) {
-    if (
-      !row.parsedValues.long ||
-      !row.parsedValues.lat ||
-      !row.parsedValues.cad_parcelles
-    ) {
-      continue;
-    }
-
-    const point = turf.point([row.parsedValues.long, row.parsedValues.lat]);
-    for (const parcelleId of row.parsedValues.cad_parcelles) {
-      const parcellesGeoJSON = cadastreGeoJSON.features.find(
-        (feature) => feature.id === parcelleId,
-      );
-      if (!parcellesGeoJSON) {
+  try {
+    for (const row of rows) {
+      if (
+        !row.parsedValues.long ||
+        !row.parsedValues.lat ||
+        !row.parsedValues.cad_parcelles
+      ) {
         continue;
       }
-
-      const lineString = turf.polygonToLine(
-        parcellesGeoJSON as Feature<Polygon>,
-      );
-
-      const distance = turf.pointToLineDistance(
-        point,
-        lineString as Feature<LineString>,
-      );
-
-      console.log('distance', distance);
-      if (distance > 1) {
-        row.errors?.push({
-          code: 'row.cadastre_outlier',
-        });
+      // On crée un point à partir des coordonnées de la ligne
+      const point = turf.point([row.parsedValues.long, row.parsedValues.lat]);
+      for (const parcelleId of row.parsedValues.cad_parcelles) {
+        // On récupère le geoJSON de la parcelle dans le cadastre
+        const parcellesGeoJSON = cadastreGeoJSON.features.find(
+          (feature) => feature.id === parcelleId,
+        );
+        if (!parcellesGeoJSON) {
+          continue;
+        }
+        // On créer un ligne a partir du polygone
+        const lineString = turf.polygonToLine(
+          parcellesGeoJSON as Feature<Polygon>,
+        );
+        // On calcule la distance entre le point et la ligne
+        const distance = turf.pointToLineDistance(
+          point,
+          lineString as Feature<LineString>,
+        );
+        // Si la distance est supérieure à 1km, on ajoute un erreur
+        if (distance > 1) {
+          row.errors?.push({
+            code: 'row.cadastre_outlier',
+          });
+        }
       }
     }
+  } catch (error) {
+    console.error('Problème lors de la validation du cadastre', error);
   }
 }
 
